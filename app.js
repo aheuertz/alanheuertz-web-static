e26e93c5f534c311ed7cd0e64b8a20029848b4f6
Alan Heuertz <aheuertz@gmail.com>
2017-01-08 14:59:48 -0700

    Feature/navigation (#1)
    * Loader directive without CSS
    
    * Include loader directive and add CSS
    
    * Remove utils directory and move to components
    
    * Use $log service for debug logging

--- app.js
+++ app.js
@@ -4,9 +4,10 @@
 	angular.module('alanheuertz', [])
 		.config(Config);
 
-	Config.$inject = ['$locationProvider'];
+	Config.$inject = ['$locationProvider', '$logProvider'];
 
-	function Config($locationProvider) {
+	function Config($locationProvider, $logProvider) {
 		$locationProvider.html5Mode(true);
+		$logProvider.debugEnabled(false);
 	}
 })();
