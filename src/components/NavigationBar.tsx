5821b12af2a5514606fcb2bbe2068533aa235efa
Alan Heuertz <aheuertz@gmail.com>
2019-04-01 23:08:05 -0400

    Re-enable nav toggle button and reposition links in nav

--- src/components/NavigationBar.tsx
+++ src/components/NavigationBar.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import {
     Navbar,
     NavbarBrand,
@@ -9,12 +9,15 @@ import {
 } from 'reactstrap';
 import { Link } from 'react-router-dom';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import NavbarToggler from 'reactstrap/lib/NavbarToggler';
 
 interface NavigationBarProps {
     title: string
 }
 
 export const NavigationBar = (props: NavigationBarProps) => {
+  const [isOpen, setIsOpen] = useState(false);
+
   const loginRedirectUri = encodeURIComponent(window.location.origin);
   // const loginClientId = '2h66cutl6qqi0bdpb3tf3lt578'; // Prod
   // const loginDomain = 'ahtest.auth.us-east-1.amazoncognito.com'; // Prod
@@ -27,21 +30,23 @@ export const NavigationBar = (props: NavigationBarProps) => {
     <div>
       <Navbar color="light" light expand="md">
         <NavbarBrand href="/">{props.title}</NavbarBrand>
-        {/* <NavbarToggler onClick={this.toggle} /> */}
-        <Collapse isOpen={true} navbar>
-          <Nav className="ml-auto" navbar>
+        <NavbarToggler onClick={() => setIsOpen(!isOpen)} />
+        <Collapse isOpen={isOpen} navbar>
+          <Nav className="mx-auto" navbar>
             <NavItem>
               <NavLink tag={Link} to="/projects">Projects</NavLink>
             </NavItem>
             <NavItem>
-            <NavLink tag={Link} to="/charts">Charts</NavLink>
+              <NavLink tag={Link} to="/charts">Charts</NavLink>
             </NavItem>
             <NavItem>
-              <NavLink tag={Link} to="/login"><FontAwesomeIcon className="mr-2" icon="user" />Log in</NavLink>
+              <NavLink tag={Link} to="/board-games">Board Games</NavLink>
             </NavItem>
-            {/* <NavItem>
-              <NavLink href={loginUri}>Log in</NavLink>
-            </NavItem> */}
+          </Nav>
+          <Nav navbar>
+            <NavItem>
+              <NavLink tag={Link} to="/login"><FontAwesomeIcon className="mr-2" icon="user" />Login</NavLink>
+            </NavItem>
           </Nav>
         </Collapse>
       </Navbar>
