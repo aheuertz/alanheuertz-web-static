be0fe14585f64d1f28671f0eb9e07e16eaf96127
Alan Heuertz <aheuertz@gmail.com>
2019-04-01 23:17:16 -0400

    Fix error page styling and remove unnecessary App component

--- src/index.tsx
+++ src/index.tsx
@@ -5,13 +5,13 @@ import 'bootstrap-material-design/dist/css/bootstrap-material-design.css';
 import { library } from '@fortawesome/fontawesome-svg-core'
 import { faSort, faSortUp, faSortDown, faUser, faExternalLinkAlt } from '@fortawesome/free-solid-svg-icons'
 import { faLinkedin, faGithub } from '@fortawesome/free-brands-svg-icons';
-import App from './App';
 import * as serviceWorker from './serviceWorker';
 import { Footer } from './components/Footer';
+import AppRouter from './AppRouter';
 
 library.add(faSort, faSortUp, faSortDown, faUser, faExternalLinkAlt, faLinkedin, faGithub);
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<AppRouter />, document.getElementById('root'));
 ReactDOM.render(<Footer />, document.getElementById('footer'));
 
 // If you want your app to work offline and load faster, you can change
--- src/pages/ErrorPage.tsx
+++ src/pages/ErrorPage.tsx
@@ -4,9 +4,9 @@ import outOfOrder from './out-of-order.png';
 const ErrorPage = () => {
   return (
     <>
-      <header className="App-header">
-        <img src={outOfOrder} className="out-of-order" alt="out of order" />
-      </header>
+      <div className="text-center">
+        <img height="500px" src={outOfOrder} className="out-of-order" alt="out of order" />
+      </div>
     </>
   )
 }
