77003d188fb951d6425beb76c37a8892a95fa41d
Alan Heuertz <aheuertz@gmail.com>
2019-04-03 17:54:39 -0400

    Print build version depending on whether buildTimestamp or buildHash are undefined

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -2,10 +2,18 @@ import React from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import Container from 'reactstrap/lib/Container';
 import Row from 'reactstrap/lib/Row';
+import { isNullOrUndefined } from 'util';
+
+function getBuildVersion(buildTimestamp: string | undefined, buildHash: string | undefined) {
+  if (isNullOrUndefined(buildTimestamp)) return buildHash;
+  if (isNullOrUndefined(buildHash)) return buildTimestamp;
+  return `${buildTimestamp}_${buildHash}`;
+}
 
 export const Footer = () => {
   const buildHash = process.env.REACT_APP_BUILD_HASH;
   const buildTimestamp = process.env.REACT_APP_BUILD_TIMESTAMP;
+  const buildVersion = getBuildVersion(buildTimestamp, buildHash);
   return (
     <Container>
       <hr />
@@ -14,7 +22,7 @@ export const Footer = () => {
         <a href="https://github.com/aheuertz" target="_blank"><FontAwesomeIcon icon={["fab", "github"]} color="gray" className="ml-2" /></a>
         <a href="https://www.linkedin.com/in/alan-heuertz-aa000713/" target="_blank"><FontAwesomeIcon icon={["fab", "linkedin"]} color="gray" className="ml-1" /></a>
       </Row>
-      <Row className="text-muted justify-content-center"><small>Build {buildHash}_{buildTimestamp}</small></Row>
+      {buildVersion && <Row className="text-muted justify-content-center"><small>Build {buildVersion}</small></Row>}
     </Container>
   )
 };
